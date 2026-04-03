5f41d2ba321e746bb5e73aedeadee73a68eadc32
vlad <vlad1slav200508@yandex.ru>
2026-04-03 11:45:10 +0300

    dummy buttons and logs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,10 +3,36 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public Button garageButton;
     public Button toCityButton;
+    public Button garageButton;
     public Button settingsButton;
     public Button creditsButton;
 
+    void Start()
+    {
+        toCityButton.onClick.AddListener(LoadCity);
+        garageButton.onClick.AddListener(OpenGarage);
+        settingsButton.onClick.AddListener(OpenSettigs);
+        creditsButton.onClick.AddListener(OpenCredits);
+    }
 
+    void LoadCity()
+    {
+        Debug.Log("Прогрузка города");
+    }
+
+    void OpenGarage()
+    {
+        Debug.Log("Перешли в гараж");
+    }
+
+    void OpenSettigs()
+    {
+        Debug.Log("Открыли настройки");
+    }
+
+    void OpenCredits()
+    {
+         Debug.Log("Экран авторов");
+    }
 }
