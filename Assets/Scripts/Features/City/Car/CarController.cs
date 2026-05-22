3a558c4304b2475e893463f8f8119492d05c6c57
vlad <vlad1slav200508@yandex.ru>
2026-05-23 01:02:10 +0300

    refactor: carcontroller

--- Assets/Scripts/Features/City/Car/CarController.cs
+++ Assets/Scripts/Features/City/Car/CarController.cs
@@ -1,7 +1,8 @@
+using OVERLIMIT.Core.Input;
 using OVERLIMIT.Core.Messages.City;
 using OVERLIMIT.Utility.Logging;
+using OVERLIMIT.Utility.Validation;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace OVERLIMIT.Features.City.Car
 {
@@ -10,21 +11,56 @@ namespace OVERLIMIT.Features.City.Car
     /// </summary>
     public class CarController : MonoBehaviour
     {
-        private void Update()
+        private Rigidbody _rb;
+        private CarInput _input;
+
+        // Ввод кэшируем в float (оси от -1.0 до 1.0)
+        private float _throttleInput; // Газ, тормоз
+        private float _steeringInput; // Лево, право
+
+        // Инициируем, awake запускается всего один раз
+        private void Awake()
         {
-            if (Keyboard.current.wKey.isPressed)
-                OverLogger.LogSuccess(CarMsg.PressedButtonForward);
-
-            if (Keyboard.current.sKey.isPressed)
-                OverLogger.LogSuccess(CarMsg.PressedButtonBack);
-
-            if (Keyboard.current.aKey.isPressed)
-                OverLogger.LogSuccess(CarMsg.PressedButtonLeft);
-
-            if (Keyboard.current.dKey.isPressed)
-                OverLogger.LogSuccess(CarMsg.PressedButtonRight);
+            _rb = GetComponent<Rigidbody>();
+            _input = new CarInput();
         }
 
-        private void FixedUpdate() { }
+        private void Start()
+        {
+            if (this.BeginValidation().Require(_rb, nameof(_rb)).LogAndCheck())
+                return;
+        }
+
+        private void OnEnable() => _input.ArcadeCar.Enable();
+
+        private void OnDisable() => _input.ArcadeCar.Disable();
+
+        // Считывает ввод игрока
+        private void Update()
+        {
+            Vector2 inputValue = _input.ArcadeCar.Movement.ReadValue<Vector2>();
+
+            _throttleInput = inputValue.y; // Газ (W/S)
+            if (inputValue.y != 0)
+                OverLogger.LogSuccess(CarMsg.MovingByY);
+
+            _steeringInput = inputValue.x; //Руль (A/D)
+            if (inputValue.x != 0)
+                OverLogger.LogSuccess(CarMsg.TurnByX);
+        }
+
+        // Работает в фиксированных кадрах, тут физика
+        private void FixedUpdate()
+        {
+            Vector3 forwardForce = transform.forward * _throttleInput * 30f;
+            _rb.AddForce(forwardForce, ForceMode.Acceleration);
+            if (forwardForce != Vector3.zero)
+                OverLogger.LogSuccess(CarMsg.AddedForce);
+
+            Vector3 turnTorque = transform.up * _steeringInput * 30f;
+            _rb.AddTorque(turnTorque, ForceMode.Acceleration);
+            if (turnTorque != Vector3.zero)
+                OverLogger.LogSuccess(CarMsg.AddedTorque);
+        }
     }
 }
