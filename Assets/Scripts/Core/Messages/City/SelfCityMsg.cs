d68ae7d701698f8b8c9a0fa8f66a35d0dc2ddb19
vlad <vlad1slav200508@yandex.ru>
2026-07-12 20:26:05 +0300

    add SpawnSuccess message

--- Assets/Scripts/Core/Messages/City/SelfCityMsg.cs
+++ Assets/Scripts/Core/Messages/City/SelfCityMsg.cs
@@ -6,5 +6,6 @@ namespace OVERLIMIT.Core.Messages.City
     public static class SelfCityMsg
     {
         public static string SelectNull = "Нет выбранной машины для спавна";
+        public static string SpawnSuccess = "Машина {spawnedCar} успешно создана";
     }
 }
