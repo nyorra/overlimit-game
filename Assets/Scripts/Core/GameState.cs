a02afe15f957680ecd60357d45a28f193079a0ea
vlad <vlad1slav200508@yandex.ru>
2026-07-12 20:20:26 +0300

    Create base game state architecture

--- Assets/Scripts/Core/GameState.cs
+++ Assets/Scripts/Core/GameState.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 namespace OVERLIMIT.Core
 {
+    public enum MachineState
+    {
+        Idle, // Покой
+        Moving, // Движение по земле
+        Flying, // В полете
+    }
+
     /// <summary>
     /// Класс для отслеживания игровых состояний
     /// </summary>
